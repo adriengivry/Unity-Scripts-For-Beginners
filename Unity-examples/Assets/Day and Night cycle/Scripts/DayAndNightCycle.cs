dd1628a1f699797516cf08ea403effeb4c4033bf
Adrien GIVRY <a.givry@student.isartdigital.com>
2018-03-22 16:51:57 -0400

    Update day and night cycle

--- Unity-examples/Assets/Day and Night cycle/Scripts/DayAndNightCycle.cs
+++ Unity-examples/Assets/Day and Night cycle/Scripts/DayAndNightCycle.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 /*
  * This script aims to provide you a very simple implementation of a day and night cycle for your unity projects
  * 
- * In order to get this script working, we need a "Directional Light" gameObject.
+ * In order to get this script working, we need a "Directional Lights" gameObject that contains sun and moon.
  * A directional light is a type of light that emits light strait forward. The sun can be
- * represented in Unity by a directional light.
- * In order to get a day and night cycle, we need to make the direction of the light rotating.
+ * represented in Unity by a directional light, the moon too.
+ * In order to get a day and night cycle, we need to make the directional light rotating.
  */
 public class DayAndNightCycle : MonoBehaviour
 {
@@ -25,17 +25,14 @@ public class DayAndNightCycle : MonoBehaviour
     }
 
     [Header("SETTING UP LINKS")]
-    [SerializeField] private GameObject m_directionalLightObject;
+    [SerializeField] private GameObject m_directionalLights;
 
     [Header("TWEAK YOUR DAY AND NIGHT CYCLE SYSTEM")]
     [SerializeField] private DayTime m_startingDayTime;
     [SerializeField] private float m_hoursPerSeconds;
 
     /* 
-     * We'll assume that the time of the day is stored as a "float" (Floating point number with single precision), in hours.
-     * Notice that in most of programming languages, including C#, you can handle floating point numbers with an higher precision
-     * using the variable type "double" (Floating point number with double precision). If you don't care about really high precision,
-     * use a float instead of double, as they needs less memory to get stored on a computer. 
+     * We'll assume that the time of the day is stored as a "float" in hours. (From 0.0f to 24.0f)
      * For 12AM we'll have 12.0f (The 'f' indicates that we are working with floats)
      */ 
     private float m_daytime;
@@ -107,12 +104,15 @@ public class DayAndNightCycle : MonoBehaviour
      * Here is where the magic happens. First we get the rotation in degrees from the daytime,
      * then we set the rotation of the directional light (the sun) to this freshly calculated rotation.
      * After that we update the time of the day by the increment setup in the inspector (Hours per seconds)
+     * We also ensure that the time of the day will not go over 24.0f
      */
     private void Update()
     {
         float rotation = ConvertDaytimeToRotation(m_daytime);
-        m_directionalLightObject.transform.rotation = Quaternion.AngleAxis(rotation, Vector3.right);
+        m_directionalLights.transform.rotation = Quaternion.AngleAxis(rotation, Vector3.right);
 
         m_daytime += m_hoursPerSeconds * Time.deltaTime;
+        while (m_daytime >= 24.0f)
+            m_daytime -= 24.0f;
     }
 }
