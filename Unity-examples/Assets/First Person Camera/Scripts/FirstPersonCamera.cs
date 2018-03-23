06722d3cadc0d60e5fff7505891c649467620731
Adrien GIVRY <adrien@givry.pro>
2018-03-23 19:22:50 -0400

    Adding a Player gameobject and starting the FPC script

---
+++ Unity-examples/Assets/First Person Camera/Scripts/FirstPersonCamera.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Here is a simple first person camera that helps you to move into your level with colliders
+ */ 
+public class FirstPersonCamera : MonoBehaviour
+{
+    [SerializeField] private float m_maxSpeed;
+
+    private float m_currentSpeed;
+
+    private void Start()
+    {
+        
+    }
+}
